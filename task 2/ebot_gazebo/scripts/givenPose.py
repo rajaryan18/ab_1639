e03fae731b82314e0344d746086a89850d92bff2
Raj Aryan <73771884+rajaryan18@users.noreply.github.com>
2021-11-26 20:55:04 +0530

    Update givenPose.py

--- task 2/ebot_gazebo/scripts/givenPose.py
+++ task 2/ebot_gazebo/scripts/givenPose.py
@@ -8,6 +8,7 @@ import moveit_msgs.msg
 import geometry_msgs.msg
 import actionlib
 import math
+import numpy as np
 
 
 class Ur5Moveit:
@@ -17,7 +18,7 @@ class Ur5Moveit:
 
 		rospy.init_node('node_eg3_set_joint_angles', anonymous=True)
 
-		self._planning_group = "ur5_1_planning_group"
+		self._planning_group = "arm_control"
 		self._commander = moveit_commander.roscpp_initialize(sys.argv)
 		self._robot = moveit_commander.RobotCommander()
 		self._scene = moveit_commander.PlanningSceneInterface()
@@ -30,7 +31,7 @@ class Ur5Moveit:
 		self._planning_frame = self._group.get_planning_frame()
 		self._eef_link = self._group.get_end_effector_link()
 		self._group_names = self._robot.get_group_names()
-		self._box_name = ''
+		self._box_name = 'agribot'
 
         # Current State of the Robot is needed to add box to planning scene
 		self._curr_state = self._robot.get_current_state()
@@ -48,7 +49,9 @@ class Ur5Moveit:
 		rospy.loginfo(pose_values)
 
 		self._group.set_pose_target(arg_pose)
-		self._group.go(wait=True)  # wait=False for Async Move
+		self._group.set_goal_tolerance(0.01)
+		self._group.plan()
+		flag_plan = self._group.go(wait=True)  # wait=False for Async Move
 
 		pose_values = self._group.get_current_pose().pose
 		rospy.loginfo('\033[94m' + ">>> Final Pose:" + '\033[0m')
@@ -62,55 +65,59 @@ class Ur5Moveit:
 			rospy.loginfo('\033[94m' + ">>> go_to_pose() Success" + '\033[0m')
 		else:
 			rospy.logerr('\033[94m' + ">>> go_to_pose() Failed. Solution for Pose not Found." + '\033[0m')
+			
+			
+	def go_to_predefined_pose(self, arg_pose_name):
+		rospy.loginfo('\033[94m' + "Going to Pose: {}".format(arg_pose_name) + '\033[0m')
+		self._group.set_named_target(arg_pose_name)
+		plan = self._group.plan()
+		goal = moveit_msgs.msg.ExecuteTrajectoryGoal()
+		goal.trajectory = plan
+		flag_plan = self._group.go(wait=True)
+		rospy.loginfo('\033[94m' + "Now at Pose: {}".format(arg_pose_name) + '\033[0m')
 
 
     # Destructor
 	def __del__(self):
 		moveit_commander.roscpp_shutdown()
 		rospy.loginfo('\033[94m' + "Object of class Ur5Moveit Deleted." + '\033[0m')
+		
+		
+def euler_to_quaternion(yaw, pitch, roll):
+
+	qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+	qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+	qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+	qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+
+	return [qx, qy, qz, qw]
 
 
 def main():
 
-	ur5 = Ur5Moveit()
-
+	ur5 = Ur5Moveit()	
+	
+	ur5.go_to_predefined_pose("shoulder_move")
+	rospy.sleep(2)
 	ur5_pose_1 = geometry_msgs.msg.Pose()
-	ur5_pose_1.position.x = -0.817261772949
-	ur5_pose_1.position.y = -0.109110076352
-	ur5_pose_1.position.z = 0.94446979642
-	ur5_pose_1.orientation.x = -0.999999995957
-	ur5_pose_1.orientation.y = 4.37354574363e-05
-	ur5_pose_1.orientation.z = 7.85715579538e-05
-	ur5_pose_1.orientation.w = 2.12177767514e-09
-
+	ur5_pose_1.position.x = 0.0327
+	ur5_pose_1.position.y = 0.301044
+	ur5_pose_1.position.z = 0.874534
+	ur5_pose_1.orientation.x = euler_to_quaternion(-2.7, -0.0024, -3.1)[0]
+	ur5_pose_1.orientation.y = euler_to_quaternion(-2.7, -0.0024, -3.1)[1]
+	ur5_pose_1.orientation.z = euler_to_quaternion(-2.7, -0.0024, -3.1)[2]
+	ur5_pose_1.orientation.w = euler_to_quaternion(-2.7, -0.0024, -3.1)[3]
+	ur5.go_to_pose(ur5_pose_1)
+	
 	ur5_pose_2 = geometry_msgs.msg.Pose()
-	ur5_pose_2.position.x = -0.414925357653
-	ur5_pose_2.position.y = 0.284932768677
-	ur5_pose_2.position.z = 1.78027849967
-	ur5_pose_2.orientation.x = -0.199396929724
-	ur5_pose_2.orientation.y = 1.64394297608e-05
-	ur5_pose_2.orientation.z = 0.979918803013
-	ur5_pose_2.orientation.w = 6.03911583936e-05
-
-	ur5_pose_3 = geometry_msgs.msg.Pose()
-	ur5_pose_3.position.x = 0.061218702528
-	ur5_pose_3.position.y = 0.150917431354
-	ur5_pose_3.position.z = 1.20083763657
-	ur5_pose_3.orientation.x = 0.635613875737
-	ur5_pose_3.orientation.y = 0.77190802743
-	ur5_pose_3.orientation.z = 0.00233308772292
-	ur5_pose_3.orientation.w = 0.0121472162087
-
-	while not rospy.is_shutdown():
-		ur5.go_to_pose(ur5_pose_1)
-		rospy.sleep(2)
-		print("One")
-		ur5.go_to_pose(ur5_pose_2)
-		rospy.sleep(2)
-		print("Two")
-		ur5.go_to_pose(ur5_pose_3)
-		rospy.sleep(2)
-		print("Three")
+	ur5_pose_2.position.x = 0.0327
+	ur5_pose_2.position.y = 0.301044
+	ur5_pose_2.position.z = 0.674534
+	ur5_pose_2.orientation.x = euler_to_quaternion(-2.7, -0.0024, -3.1)[0]
+	ur5_pose_2.orientation.y = euler_to_quaternion(-2.7, -0.0024, -3.1)[1]
+	ur5_pose_2.orientation.z = euler_to_quaternion(-2.7, -0.0024, -3.1)[2]
+	ur5_pose_2.orientation.w = euler_to_quaternion(-2.7, -0.0024, -3.1)[3]
+	ur5.go_to_pose(ur5_pose_2)
 
 	del ur5
 
