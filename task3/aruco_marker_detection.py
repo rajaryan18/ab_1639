48514db811d4d18f4c9e5b824595dc28ea9da6aa
Raj Aryan <73771884+rajaryan18@users.noreply.github.com>
2021-12-10 22:32:07 +0530

    subscribed to depth camera

--- task3/aruco_marker_detection.py
+++ task3/aruco_marker_detection.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python3
-
-"""
-Description: Obtaining TF of a ArUco marker using single sjcam camera
-Algorithm: 
-	Input: ROS topic for the RGB image
-	Process: 
-        - Subscribe to the image topic
-		- Convert ROS format to OpenCV format
-		- Detecting ArUco marker along with its ID
-		- Applying perspective projection to calculate focal length
-		- Extracting depth for each aurco marker
-		- Broadcasting TF of each ArUco marker with naming convention as aruco1 for ID1, aruco2 for ID2 and so on.
-	Output: TF of ArUco marker with respect to ebot_base
-"""
-
 import cv2 as cv
 import numpy as np
 import roslib
@@ -26,13 +11,30 @@ import geometry_msgs.msg
 import tf_conversions
 
 cv_image = None
+cf_u = 0
+cf_v = 0
+cf_d = 0
+
+
+def depth_callback(msg):
+    global cf_d, cf_u, cf_v
+    # create OpenCV depth image using defalut passthrough encoding
+    try:
+        depth_image = CvBridge().imgmsg_to_cv2(msg, desired_encoding='passthrough')
+    except CvBridgeError as e:
+        print(e)
+
+    # using green ball (u, v) position, find depth value of Crazyflie point and divide by 1000
+    # to change millimeters into meters (for Kinect sensors only)
+    cf_d = depth_image[cf_v, cf_u]/1.0
+
 
 def callback(data):
     # Initializing variables
-    global cv_image
-    focal_length = 476.70308
-    center_x = 400.5
-    center_y = 400.5
+    global cv_image, cf_d
+    focal_length = 554.387
+    center_x = 320.5
+    center_y = 240.5
     aruco_dimension = 0.1
     try:
         bridge = CvBridge()
@@ -49,28 +51,29 @@ def callback(data):
 
         for c in contours:
             area = cv.contourArea(c)
-            if(area>100):
+            if(area>200):
                 x, y, w, h = cv.boundingRect(c)
-                frame = cv.rectangle(frame, (x, y-5), (x+w, y+h+5), (255, 0, 0), 1)
+                frame = cv.rectangle(frame, (x, y-10), (x+w, y+h+10), (255, 0, 0), 1)
                 
                 pixel_width = h
                 cX = (x + x+w) // 2
                 cY = (y + y+h) // 2
+                frame = cv.circle(frame, (cX, cY), 1, (255, 255, 255), 1)
 
                 # obtain depth for each ArUco marker
                 distance = (focal_length*aruco_dimension)/pixel_width
-
+                rospy.Subscriber("/camera/depth/image_raw2", Image, depth_callback)
                 # transforming pixel coordinates to world coordinates
-                world_x = (cX - center_x)/focal_length*distance
-                world_y = (cY - center_y)/focal_length*distance
-                world_z = distance
+                world_x = ((cX - center_x)/focal_length)*cf_d
+                world_y = ((cY - center_y)/focal_length)*cf_d
+                world_z = cf_d
 
                 # broadcasting TF for each aruco marker
                 br = tf2_ros.TransformBroadcaster()
                 t = geometry_msgs.msg.TransformStamped()
                 t.header.stamp = rospy.Time.now()
-                t.header.frame_id = "sjcam_link"
-                t.child_frame_id = "aruco"+str(x)
+                t.header.frame_id = "camera_rgb_frame2"
+                t.child_frame_id = "Ob"+str(x)
 
                 # putting world coordinates coordinates as viewed for sjcam frame
                 t.transform.translation.x = world_z
@@ -84,8 +87,6 @@ def callback(data):
                 t.transform.rotation.w = q[3]
 
                 br.sendTransform(t)
-
-        '''uncoment to view the visual of detection'''
         cv.imshow("frame", frame)
         cv.waitKey(1)
     except CvBridgeError as e:
